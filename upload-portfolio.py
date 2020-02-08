954704ce3e4f5f41940dc8dbe9f21010c2f74f67
Andrew Tyler <andrewtyler@Andrews-MacBook-Pro.local>
2020-02-08 18:07:39 +0800

    Testing the change to the image

--- upload-portfolio.py
+++ upload-portfolio.py
@@ -9,15 +9,33 @@ import boto3
 def lambda_handler(event, context):
    
     try:
+        location = {
+            "bucketName" : 'andrewmtyler-portfolio-build',
+            "objectKey" : 'portfoliobuild.zip'
+        }
+        
         s3 = boto3.resource("s3")
         portfolio_bucket = s3.Bucket("andrewmtyler-portfolio")
-        build_bucket =s3.Bucket('andrewmtyler-portfolio-build')
-         
+        
+        
         sns = boto3.resource('sns')
         topic = sns.Topic('arn:aws:sns:us-east-1:822803490390:PortfolioUpdated')
         
+        job = event.get("CodePipeline.job")
+        
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                print ('Found Artifact')
+                print (artifact["name"])
+                if artifact["name"] == "BuildArtifact":
+                    location = artifact["location"]["s3Location"]
+                    
+        print ('Building portfolio from location ' + str(location))
+                    
+        build_bucket =s3.Bucket(location["bucketName"])
+        
         portfolio_zip = BytesIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
         
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -28,6 +46,9 @@ def lambda_handler(event, context):
                 
                 
         topic.publish(Subject="Portfolio Updated", Message="Your portfolio has been updated")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job['id'])
     
     except:
         topic.publish(Subject="Portfolio Update Failed", Message="The portfolio update has failed")
